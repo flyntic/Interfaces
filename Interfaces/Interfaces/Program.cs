9ae2b975fb38e13efd0d48a304a0ede4ac439284
flyntic <64376713+flyntic@users.noreply.github.com>
2022-04-20 20:24:00 +0300

    Add files via upload

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,68 @@
+﻿using Interfaces;
+
+Director director = new Director()
+{
+    LastName = "Иванов ",
+    Name = "Петр",
+    Birthday = new DateTime(1990, 9, 9),
+    Position = "директор",
+    Salary = 1000
+
+};
+
+IWorker seller = new Seller()
+{
+    LastName = "Димилин",
+    Name = "Димка",
+    Birthday = new DateTime(1999, 9, 9),
+    Position = "продавец",
+    Salary = 12.9
+
+};
+
+if (seller is Employee )
+{
+    Console.WriteLine($"Зарплата продавца : {(seller as Employee).Salary} ");
+    
+}
+
+
+
+director.ListOfWorkers = new List<IWorker>()
+{
+    seller,
+    new Cashier
+    {
+        LastName = "Димилин1",
+        Name = "Димка1",
+        Birthday = new DateTime(1999, 9, 9),
+        Position = "кассир",
+        Salary = 2.9
+
+
+    },
+    new Keeper()
+    {
+        LastName = "Димилин",
+        Name = "Димка2",
+        Birthday = new DateTime(1999, 9, 9),
+        Position = "кладовщик",
+        Salary = 1.9
+
+    }
+
+};    
+    
+Console.WriteLine(director);
+
+if (director is IManager)
+{
+   director.Control(); 
+}
+
+
+foreach (IWorker i in director.ListOfWorkers)
+{   Console.WriteLine(i);
+    if (i.IsWorking)
+    Console.WriteLine(i.Work());
+}
---
+++ Interfaces/Interfaces/Samples.cs
@@ -0,0 +1,110 @@
+﻿namespace Interfaces;
+
+public abstract class Human
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public DateTime Birthday { get; set; }
+
+    public override string ToString()
+    {
+        return $"Фамилия {LastName} \nИмя {Name}\nДень рождения {Birthday.ToLongDateString()}\n ";
+    }
+
+}
+
+public abstract class Employee : Human
+{
+    public string Position { get; set; }
+    public double Salary   { get; set; }
+
+    public override string ToString()
+    {
+        return base.ToString() + $"Должность {Position}\nЗарплата {Salary}\n";
+    }
+    
+}
+
+public interface IWorker
+{
+    bool IsWorking { get; }
+    string Work();
+}
+
+public interface IManager
+{
+    List<IWorker> ListOfWorkers { get; set; }
+    void Organize();
+    void Control();
+    void MakeBudget();
+}
+
+class Director : Employee, IManager
+{
+    public List<IWorker> ListOfWorkers { get; set; }
+    public void Organize()
+    {
+        Console.WriteLine("Организовывает");
+    }
+
+    public void Control()
+    {
+        Console.WriteLine("Контролирует");
+    }
+
+    public void MakeBudget()
+    {
+        Console.WriteLine("Бюджетирует");
+    }
+}
+
+class Seller : Employee, IWorker
+{
+    private bool isWorking = true;
+
+    public bool IsWorking
+    {
+        get
+        {
+            return isWorking;
+        }
+    }
+    public string Work()
+    {
+        return $"Продаю товар";
+    }
+}
+
+class Cashier : Employee, IWorker
+{
+    private bool isWorking = true;
+
+    public bool IsWorking
+    {
+        get
+        {
+            return isWorking;
+        }
+    }
+    public string Work()
+    {
+        return $"Принимаю оплату за товар";
+    }
+}
+
+class Keeper: Employee, IWorker
+{
+    private bool isWorking = true;
+
+    public bool IsWorking
+    {
+        get
+        {
+            return isWorking;
+        }
+    }
+    public string Work()
+    {
+        return $"Выдаю товар";
+    }
+}
